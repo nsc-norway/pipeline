13b8d24fd891a94a2169fee65dbff3dbd4c4f528
Marius Bjørnstad <p.m.bjornstad@medisin.uio.no>
2015-04-20 08:15:55 +0200

    don|t need setup routine for qc -- but do need it for nextseq demux

--- setup-qc.py
+++
@@ -1,9 +0,0 @@
-# Script for setting QC options 
-
-
-# ??? TODO : how to structure this? can I use epp-submit-slurm or should I make a submit-qc?
-
-
-
-if __name__ == "__main__":
-    main(sys.argv[1])
