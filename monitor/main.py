ba56341a9d3b3626f9637553b1fccb89b04ccf13
Marius Bjørnstad <p.m.bjornstad@medisin.uio.no>
2015-05-07 09:23:41 +0200

    some work on monitoring

---
+++ monitor/main.py
@@ -0,0 +1,51 @@
+from flask import Flask
+from genologics.lims import *
+from genologics import config
+import datetime
+
+app = Flask(__name__)
+
+
+class Section(object):
+    def __init__(self, name):
+        self.name = name
+
+
+def get_pending():
+    pass
+
+
+def get_sample_prep():
+    pass
+
+
+def get_library_prep():
+    pass
+
+
+def get_sequencing():
+    pass
+
+def get_data_processing():
+    pass
+
+
+@app.route('/')
+def get_main():
+    lims = Lims(config.BASEURI, config.USERNAME, config.PASSWORD)
+    active_projects = lims.get_projects(udf={'Status': 'Open'})
+    show_date = datetime.date.today + datetime.timedelta(days=-30)
+    completed_projects = lims.get_projects(
+            open_date=show_date,
+            udf={'Status': 'Delivered'}
+            )
+
+    pending = Section("Pending")
+    sample_prep = get_sample_prep()
+
+
+
+    return render_template('project-list.xhtml', server="ous-lims")
+
+if __name__ == '__main__':
+    app.run()
