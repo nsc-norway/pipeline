109c99ba761ef28b63457a56faaf3f8f5e7a3eab
Marius Bjørnstad <p.m.bjornstad@medisin.uio.no>
2015-05-08 08:30:44 +0200

    irrelevant changes, monitoring

--- monitor/main.py
+++ monitor/main.py
@@ -11,8 +11,10 @@ class Section(object):
         self.name = name
 
 
-def get_pending():
-    pass
+def get_pending(active_projects):
+    for p in active_projects:
+        if p.TODO:
+            pass
 
 
 def get_sample_prep():
@@ -33,18 +35,18 @@ def get_data_processing():
 @app.route('/')
 def get_main():
     lims = Lims(config.BASEURI, config.USERNAME, config.PASSWORD)
-    active_projects = lims.get_projects(udf={'Status': 'Open'})
+    active_projects = lims.get_projects(udf={'Progress!': 'Delivered'})
     show_date = datetime.date.today + datetime.timedelta(days=-30)
     completed_projects = lims.get_projects(
             open_date=show_date,
-            udf={'Status': 'Delivered'}
+            udf={'Progress': 'Delivered'}
             )
 
-    pending = Section("Pending")
+    pending = get_pending(active_projects)
     sample_prep = get_sample_prep()
 
 
-
+    # TODO: how to pass the parameters
     return render_template('project-list.xhtml', server="ous-lims")
 
 if __name__ == '__main__':
