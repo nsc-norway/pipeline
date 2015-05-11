ce9d3fe30255c1b1946f4238af873e078641dce6
Marius Bjørnstad <p.m.bjornstad@medisin.uio.no>
2015-05-11 19:02:52 +0200

    setting up the structure of the "cockpit"

--- monitor/main.py
+++ monitor/main.py
@@ -6,24 +6,22 @@ import datetime
 app = Flask(__name__)
 
 
-class Section(object):
-    def __init__(self, name):
+class ProjectInfo(object):
+    """Represents the progress / status of each project"""
+
+    def __init__(
+            self, name, completed_date = None,
+            step = None, status = None, total = None
+            ):
         self.name = name
+        self.completed_date = completed_date
+        self.step = step
+        self.status = status
+        self.total = total
 
 
-def get_pending(active_projects):
-    for p in active_projects:
-        if p.TODO:
-            pass
-
-
-def get_sample_prep():
+def get_before_sequencing():
     pass
-
-
-def get_library_prep():
-    pass
-
 
 def get_sequencing():
     pass
@@ -31,19 +29,29 @@ def get_sequencing():
 def get_data_processing():
     pass
 
+def get_delivery():
+    pass
+
+def get_completed_projects(projects):
+    return [
+            ProjectInfo(pro.name, pro.close_date) for pro in projects
+            ]
+
 
 @app.route('/')
 def get_main():
     lims = Lims(config.BASEURI, config.USERNAME, config.PASSWORD)
-    active_projects = lims.get_projects(udf={'Progress!': 'Delivered'})
+    active_projects = lims.get_projects(udf={'Progress': 'Delivered'})
     show_date = datetime.date.today + datetime.timedelta(days=-30)
     completed_projects = lims.get_projects(
             open_date=show_date,
             udf={'Progress': 'Delivered'}
             )
 
-    pending = get_pending(active_projects)
-    sample_prep = get_sample_prep()
+    pending = get_before_sequencing(active_projects)
+    sequencing = get_sequencing(active_projects)
+    data = get_data_processing()
+    completed = get_completed_projects(completed_projects)
 
 
     # TODO: how to pass the parameters
