5f7f11a259dbdc115b3dc24f9188d60b98fef9b9
Marius Bjørnstad <p.m.bjornstad@medisin.uio.no>
2015-05-13 09:09:44 +0200

    removing dead code

--- common/parse.py
+++ common/parse.py
@@ -28,29 +28,6 @@ def get_lane_cluster_density(path):
 
 ###################### HISEQ METRICS #######################
 
-def parse_demux_stats(stats_data):
-    """Parse the Demultiplex_stats.htm file and return a list of records,
-    one for each row."""
-
-    # re.DOTALL does a "tall" match, including multiple lines
-    tables = re.findall("<table[ >].*?</table>", stats_data, re.DOTALL)
-
-    header_table = tables[0]
-    field_names = []
-    for match in re.finditer("<th>(.*?)</th>", header_table):
-        field_names.append(match.group(1))
-
-    barcode_lane_table = tables[1]
-    barcode_lanes = []
-    for row in re.finditer("<tr>(.*?)</tr>", barcode_lane_table, re.DOTALL): 
-        barcode_lane = {}
-        for i, cell in enumerate(re.finditer("<td>(.*?)</td>", row.group(1))):
-            barcode_lane[field_names[i]] = cell.group(1)
-        barcode_lanes.append(barcode_lane)
-
-    return barcode_lanes
-
-
 def to_normal_dict(dd):
     '''Recursive function to convert a tree of defaultdict to a normal dict'''
     if isinstance(dd, defaultdict):
