75faa1b67d6a12e226819d9b0e49c3bfbab5cf2d
Marius Bjørnstad <p.m.bjornstad@medisin.uio.no>
2015-05-18 11:33:41 +0200

    just a bunch of comments

--- monitor/main.py
+++ monitor/main.py
@@ -3,6 +3,44 @@ from genologics.lims import *
 from genologics import config
 import datetime
 
+# Project / Sample progress tagging
+# ---------------------------------
+# The Progress UDF of the Sample is set by Clarity LIMS on various stages.
+
+
+# Obviously, different samples can be in different workflows
+# Project 
+# |- Sample 1 -> W1
+# |- Sample 2 -> W1, W2
+# \- Sample 3 -> W2
+
+# Project 
+# |- Sample 1
+# |- Sample 2
+# |          |- Artifact 1 in workflow 1
+# |          |- Artifact 2 in workflow 2
+# |          \- Artifact 3 in workflow 1
+# \- Sample 3
+
+# When a (submitted) sample is added to a workflow, an artifact for that sample is 
+# automatically generated. Each artifact can have derived samples for them, which
+# can be the objects taken further into the workflow. E.g. one stops working on 
+# the original artifact and starts working on a pool. Derived samples can also be
+# added to workflows (TODO: figure out exactly what happens).
+
+# 1) How to define the progress of a project:
+#    The project may have multiple progresses. Each artifact in a queue or in use
+#    in a step (ice bucket) should be represented as a progress level. A completed
+#    workflow should also be considered as the progress.
+# 2) Special case: With respect to the Project Evaluation workflow, the project is
+#    either evaluated or pending. If any other work has been done on the project, 
+#    the evaluated or pending state is no longer relevant.
+
+# Algorithm for determining the progress:
+
+
+
+
 app = Flask(__name__)
 
 
