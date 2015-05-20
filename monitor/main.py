79b339931e0772d2b63001788b545727d3b777fe
Marius Bjoernstad <p.m.bjornstad@medisin.uio.no>
2015-05-20 18:21:08 +0200

    some initial development on monitoring page"

--- monitor/main.py
+++ monitor/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from genologics.lims import *
+import re
 import nsc
 import datetime
 import threading
@@ -27,7 +28,7 @@ INSTRUMENTS = ["HiSeq", "NextSeq", "MiSeq"]
 SEQUENCING = [
         ("Illumina SBS (HiSeq GAIIx) 5.0", "Illumina Sequencing (Illumina SBS) 5.0"),
         ("Illumina SBS (NextSeq) 1.0", "NextSeq Run (NextSeq) 1.0"),
-        ("Illumina SBS (NextSeq) 1.0", "NextSeq Run (NextSeq) 1.0")
+        ("Illumina SBS (MiSeq) 5.0", "MiSeq Run (MiSeq) 5.0")
         ]
 
 # [ (Protocol, (Step, Step, Step)) ]
@@ -78,7 +79,8 @@ class QueueState(object):
         self.num = num
 
 class SimpleSequencerRun(object):
-    def __init__(self, project):
+    def __init__(self, url, project):
+        self.url = url
         self.project = project
 
 
@@ -114,19 +116,23 @@ def get_processes(process_name):
     return result
 
 
+def proc_url(process_id):
+    global ui_server
+    second_part_limsid = re.match(r"[\d]+-([\d]+)$", process_id).group(1)
+    return "{0}clarity/work-details/{1}".format(ui_server, second_part_limsid)
+
 def read_mi_next_seq(process):
     try:
         project = process.all_inputs()[0].samples[0].project.name
     except IndexError:
         return SimpleSequencerRun("")
-
-    return SimpleSequencerRun(project)
+    return SimpleSequencerRun(proc_url(process.id), project)
 
 def read_hiseq(process):
     return None
 
 
-def get_sequencing(protocol, process_name, read_function):
+def get_sequencing(process_name, read_function):
     procs = get_processes(process_name)
     return [read_function(proc) for proc in procs]
 
@@ -139,10 +145,20 @@ def get_completed_projects(projects):
 
 @app.route('/')
 def get_main():
-    
+    global ui_server
+
+    ui_servers = {
+            "http://dev-lims.ous.nsc.local:8080/": "https://dev-lims.ous.nsc.local/",
+            "http://ous-lims.ous.nsc.local:8080/": "https://ous-lims.ous.nsc.local/"
+            }
+    try:
+        ui_server = ui_servers[nsc.lims.baseuri]
+    except KeyError:
+        ui_server = nsc.lims.baseuri
+
     seq_queues = [get_queue(*proc) for proc in SEQUENCING]
-    seq = [get_sequencing(SEQUENCING[0], read_hiseq)] +\
-            [get_sequencing(sp, read_mi_next_seq) for sp in SEQUENCING[1:]]
+    seq = [get_sequencing(SEQUENCING[0][1], read_hiseq)] +\
+            [get_sequencing(sp[1], read_mi_next_seq) for sp in SEQUENCING[1:]]
 
     show_date = datetime.date.today() + datetime.timedelta(days=-COMPLETED_DAYS)
     #completed_projects = lims.get_projects(
@@ -156,7 +172,7 @@ def get_main():
     #completed = get_completed_projects(completed_projects)
 
 
-    ## TODO: how to pass the parameters
+    print seq
     return render_template(
             'project-list.xhtml',
             server=nsc.lims.baseuri,
