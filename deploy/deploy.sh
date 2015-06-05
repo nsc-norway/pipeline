8d3b26ee848cd8fd44fdf9e74bc64390e88704c0
Marius Bjørnstad <p.m.bjornstad@medisin.uio.no>
2015-06-05 08:46:34 +0200

    initial version of the deployment script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 set -e
 
+# Deployment script for OUS closed network
+# Don't even attempt to be 
+
 REPOS=pipeline genologics
-DIR=1
-for repo in REPOS
+INSTALLATIONS=/data/nsc.loki/automation /var/www/limsweb
+
+# Deploy to installations from dev area
+for installation in INSTALLATIONS
 do
-	mv /data/nsc.loki/automation/$repo /data/nsc.loki/automation/$repo.2
-	mkdir /data/nsc.loki/automation/$repo
-	git archive $1 | (pushd 
+	for repo in REPOS
+	do
+		deploy_dir=$installation/$repo
+		mv $deploy_dir $deploy_dir.2
+		mkdir $deploy_dir
+		source_dir=/data/nsc.loki/automation/dev/$repo
+		pushd $source_dir > /dev/null
+		git archive $1 | (pushd $deploy_dir; tar vx ; popd)
+		chmod -R a+rX $deploy_dir
+		chgrp -R nsc-seq $deploy_dir
+		popd > /dev/null
+		rm -rf $deploy_dir.2
+	done
 done
