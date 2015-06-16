e9e1739a826f17a17d6aac22bcd32089ea31b6db
Marius Bjørnstad <p.m.bjornstad@medisin.uio.no>
2015-06-16 20:19:09 +0200

    changing the condition for finalising a run
    only require qc-ok inputs.
    only move to processed if all ok.

--- finalize-run.py
+++ finalize-run.py
@@ -12,37 +12,48 @@ from genologics.lims import *
 from common import nsc, parse, utilities
 
 
+def finish(process, fc):
+    try:
+        del fc.udf[nsc.AUTO_FLOWCELL_UDF]
+    except KeyError:
+        pass # in case it was not marked for automation
+    fc.udf[nsc.RECENTLY_COMPLETED_UDF] = True
+    fc.udf[nsc.PROCESSED_DATE_UDF] = date.today()
+    fc.put()
+
+    seq_process = utilities.get_sequencing_process(process)
+    runid = seq_process.udf['Run ID']
+    if all(input.qc_flag == 'PASSED' for lane in fc.placements.values()):
+        print "Moving", runid, "to processed directory"
+        os.rename(
+                os.path.join(nsc.PRIMARY_STORAGE, runid),
+                os.path.join(nsc.PRIMARY_STORAGE, "processed", runid)
+                )
+
+
+
+
 def main(process_id):
     process = Process(nsc.lims, id=process_id)
     utilities.running(process)
 
     inputs = process.all_inputs(unique=True)
     flowcells = set(i.location[0] for i in inputs)
-    if len(flowcells) == 1 and \
-            len(inputs) == next(iter(flowcells)).occupied_wells:
-
+    if len(flowcells) == 1:
         fc = next(iter(flowcells))
         fc.get()
-        try:
-            del fc.udf[nsc.AUTO_FLOWCELL_UDF]
-        except KeyError:
-            pass # in case it was not marked for automation
-        fc.udf[nsc.RECENTLY_COMPLETED_UDF] = True
-        fc.udf[nsc.PROCESSED_DATE_UDF] = date.today()
-        fc.put()
 
-        seq_process = utilities.get_sequencing_process(process)
-        runid = seq_process.udf['Run ID']
-        if all(input.qc_flag == 'PASSED' for lane in inputs):
-            print "Moving", runid, "to processed directory"
-            os.rename(
-                    os.path.join(nsc.PRIMARY_STORAGE, runid),
-                    os.path.join(nsc.PRIMARY_STORAGE, "processed", runid)
-                    )
-
-        utilities.success_finish(process)
+        # Don't run if not all QC pass lanes are here
+        fc_lanes = fc.placements.values()
+        ok_lanes = set(l for l in fc_lanes if l.qc_flag == 'PASSED')
+        
+        if set(inputs) >= ok_lanes:
+            finish(process, fc)
+            utilities.success_finish(process)
+        else:
+            utilities.fail(process, "Need all QC passed inputs of a single flowcell as input")
     else:
-        utilities.fail(process, "Need all inputs of a single flowcell as input")
+        utilities.fail(process, "Only one flowcell allowed as input")
 
 
 with utilities.error_reporter():
