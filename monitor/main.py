96a717473b60af6bc0c98abff6efd6633bf8aac5
Marius Bjoernstad <p.m.bjornstad@medisin.uio.no>
2015-06-16 11:50:55 +0200

    fixing a bug?

--- monitor/main.py
+++ monitor/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, Response
+from flask import Flask, render_template, url_for, request, Response, redirect
 from genologics.lims import *
 import re
 import requests
