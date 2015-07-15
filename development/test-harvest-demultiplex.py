4434dd50faa8ea365f6e3e3d67b85a73a1ea046f
Marius Bjoernstad <marius.bjornstad@medisin.uio.no>
2015-07-15 11:54:28 +0200

    removing obsolete development scripts

--- development/test-harvest-demultiplex.py
+++
@@ -1,7 +0,0 @@
-import sys, os.path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-from genologics.lims import *
-from common import nsc, demultiplex
-
-demultiplex.populate_results(Process(nsc.lims, id = sys.argv[1]), sys.argv[2])
-
--- development/test-parsing.py
+++
@@ -1,9 +0,0 @@
-import sys, os.path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-from genologics.lims import *
-from common import nsc, parse
-
-stats = parse.get_nextseq_stats("../../ns")
-print stats[(1, "unknown", 1)]
-
-
