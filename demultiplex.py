011de9c8174e8e7dad0a078f4513570cca7eaa01
Marius Bjoernstad <marius.bjornstad@medisin.uio.no>
2015-07-15 16:45:34 +0200

    continuing to move stuff

--- demultiplex.py
+++ demultiplex.py
@@ -1,1 +1,23 @@
 
+
+def download_sample_sheet(process, save_dir, append_limsid=True):
+    """Downloads the demultiplexing process's sample sheet, which contains only
+    samples for the requested project (added to the LIMS by setup-*-demultiplexing.py)."""
+
+    sample_sheet = None
+    for o in process.all_outputs(unique=True):
+        if o.output_type == "ResultFile" and o.name == "SampleSheet csv":
+            if len(o.files) == 1:
+                sample_sheet = o.files[0].download()
+
+    if sample_sheet:
+        if append_limsid:
+            name = "SampleSheet-" + process.id + ".csv"
+        else:
+            name = "SampleSheet.csv"
+        path = os.path.join(save_dir, name)
+        file(path, 'w').write(sample_sheet)
+        return path, sample_sheet
+    else:
+        return None, None
+
