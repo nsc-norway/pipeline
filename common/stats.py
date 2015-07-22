bfb7bbc3a8040aeeac6208ec3efc45e35f63de5c
Marius Bjoernstad <marius.bjornstad@medisin.uio.no>
2015-07-22 13:16:09 +0200

    moving stats gathering into stats.py

--- common/stats.py
+++ common/stats.py
@@ -1,5 +1,13 @@
 # Parsing of bcl2fastq2 output and Illumina Analysis Software output for the MiSeq
 
+import re
+import os
+import itertools
+from xml.etree import ElementTree
+from collections import defaultdict
+from Counter import Counter
+
+
 def parse_conversion_stats(conversion_stats_path, aggregate_lanes, aggregate_reads):
     """Get "conversion stats" from the bcl2fastq2 stats.
 
@@ -255,4 +263,169 @@ def get_nextseq_stats(stats_xml_file_path, aggregate_lanes=True, aggregate_reads
         result[coordinates] = stats
 
     return result
+
+
+
+
+
+###################### MISEQ STATS #######################
+
+def get_miseq_totals(run_stats):
+    """Get the run totals for miseq. Argument is the RunStats XML element
+    from the GenerateFASTQRunStatistics.xml file."""
+
+    totals = {}
+    for tag in [
+            'NumberOfClustersPF',
+            'NumberOfClustersRaw',
+            'NumberOfUnalignedClusters',
+            'NumberOfUnalignedClustersPF',
+            'NumberOfUnindexedClusters',
+            'NumberOfUnindexedClustersPF',
+            ]:
+        totals[tag] = int(run_stats.findall(tag)[0].text)
+
+    return totals
+
+
+
+def parse_generate_fastq(generate_fastq_path, num_reads=1, aggregate_reads=False): 
+    """Get read-level demultiplexing statistics from GenerateFASTQRunStatistics.xml.
+    
+    (Modelled on code for the HiSeq)
+    
+    Statistics gathered:
+        - per sample
+        - pass filter / raw
+
+    Per read stats not actually gathered, but we'll return multiple entries if
+    per-read stats requested.
+    
+    The return value is:
+    totals_dict, main_dict.
+
+    
+    totals_dict represents the stats for the full run (unaligned),
+    main_dict represents the per-sample stats and undetermined.
+
+    The main_dict is indexed by (sample_id,) (tuple with one element) if
+    aggregate_reads is set, or (sample_id, read) if aggregate_reads is not set.
+    { (lane, sample id,) => { stat name => value } }
+    { (lane, sample id, read) => { stat name => value } }
+    lane is always 1 (included to have same format as for HiSeq)
+    
+    Undetermined is represented as sample_id = None.
+    """
+
+    xmltree = ElementTree.parse(generate_fastq_path)
+    root = xmltree.getroot()
+    if root.tag != "StatisticsGenerateFASTQ":
+        raise RuntimeError("Expected XML element StatisticsGenerateFASTQ, found " + root.tag)
+
+    totals_dict = get_miseq_totals(root.findall("RunStats")[0])
+
+    samples = {}
+    # Undetermined first
+    for i_read in range(1, num_reads+1):
+        stats = {}
+        stats['NumberOfClustersRaw'] = totals_dict['NumberOfUnindexedClusters']
+        stats['NumberOfClustersPF'] = totals_dict['NumberOfUnindexedClustersPF']
+        samples[(1, None, i_read)] = stats
+
+    overall = root.findall("OverallSamples")[0]
+    for sample_xml in overall.findall("SummarizedSampleStatistics"):
+        sample_id = sample_xml.findall("SampleID")[0].text
+
+        # Needs to be changed if we actually get per-read stats later, but for
+        # now they are just dummy values in the XML.
+        if aggregate_reads:
+            indexes = [(sample_id,)]
+            factor = num_reads
+        else:
+            indexes = [(1, sample_id, i) for i in range(1, num_reads+1)]
+            factor = 1
+
+        sample_stats = {}
+        for tag in [
+                "NumberOfClustersRaw",
+                "NumberOfClustersPF",
+                ]:
+            sample_stats[tag] = int(sample_xml.findall(tag)[0].text) * factor
+
+        for index in indexes:
+            samples[index] = sample_stats
+
+    
+    return totals_dict, samples
+
+
+
+def get_miseq_stats(generate_fastq_path, num_reads, aggregate_reads):
+    """Get the MiSeq demultiplexing stats.
+
+    It returns a dict indexed by (sample ID,) or (sample ID, read)
+    with the values being a dict indexed by the stats name:
+    { (sample ID,) => {stat => value} }
+    { (sample ID, read) => {stat => value} }
+
+    Sample ID None represents Undetermined.
+
+    No info is actually gathered per read, the stats are duplicated for 
+    easier processing. The miseq info is quite limited compared to hi/nextseq, 
+    but at least we get the number of clusters, which is the main thing that's 
+    needed.
+    """
+
+    overall, samples = parse_generate_fastq(
+            generate_fastq_path,
+            num_reads,
+            aggregate_reads
+            )
+
+    # Check: compute totals by summing up samples
+    all_raw_clusters = sum(
+            d["NumberOfClustersRaw"]
+            for c,d in samples.items()
+            if c[-1] == 1 # Read 1, or sum of reads if aggregate
+            )
+    all_pf_clusters = sum(
+            d["NumberOfClustersPF"]
+            for c,d in samples.items()
+            if c[-1] == 1
+            )
+
+    # Check that stats for all samples add up to the totals
+    if aggregate_reads:
+        factor = num_reads
+    else:
+        factor = 1
+    num_raw = overall["NumberOfClustersRaw"] * factor
+    num_pf = overall["NumberOfClustersPF"] * factor
+    assert(all_raw_clusters == num_raw)
+    assert(all_pf_clusters == num_pf)
+
+    result = {}
+    for coordinates, xml_stats in samples.items():
+        stats = {}
+        try:
+            # Note: if aggregate is set, this is no longer the actual number of clusters, but
+            # the number of clusters times the number of read passes
+            stats['# Reads'] = xml_stats['NumberOfClustersRaw']
+            stats['# Reads PF'] = xml_stats['NumberOfClustersPF']
+            if xml_stats['NumberOfClustersRaw'] > 0:
+                stats['%PF'] = xml_stats['NumberOfClustersPF'] * 100.0 / xml_stats['NumberOfClustersRaw']
+            else:
+                stats['%PF'] = "100.0%"
+            stats['% of Raw Clusters Per Lane'] = xml_stats['NumberOfClustersRaw'] * 100.0 / all_raw_clusters
+            stats['% of PF Clusters Per Lane'] = xml_stats['NumberOfClustersPF'] * 100.0 / all_pf_clusters
+        except ZeroDivisionError:
+            print "Warning: division by zero"
+        result[coordinates] = stats
+
+    return result
+
+
+
+
+
 
