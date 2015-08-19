080f822cac6e3e28740e24affb48113538640a84
Marius Bjoernstad <marius.bjornstad@medisin.uio.no>
2015-08-19 16:21:12 +0200

    first version of "processed" script, to replace finalize...

--- 40_processed.py
+++ 40_processed.py
@@ -1,3 +1,51 @@
 # Move run into processed/ on primary storage, and mark as completed
 # in LIMS 
-# TODO
+
+import sys
+import os
+import subprocess
+from datetime import date
+from genologics.lims import *
+from common import nsc, parse, utilities, taskmgr
+
+TASK_NAME = "Mark processed"
+TASK_DESCRIPTION = """Do some bookkeeping and move original run folder into processed/ 
+                    directory on primary storage, since we are done with it."""
+TASK_ARGS = ['src_dir']
+
+
+def main(task):
+    task.running()
+
+    runid = task.run_id
+
+    if task.process:
+        inputs = process.all_inputs(unique=True)
+        flowcells = set(i.location[0] for i in inputs)
+        if len(flowcells) == 1:
+            fc = next(iter(flowcells))
+            fc.get()
+            # TODO: determine how automation is tracked in v2
+            try:
+                del fc.udf[nsc.AUTO_FLOWCELL_UDF]
+            except KeyError:
+                pass
+            
+            # Tracking UDF for "overview" page
+            fc.udf[nsc.RECENTLY_COMPLETED_UDF] = True
+            fc.udf[nsc.PROCESSED_DATE_UDF] = date.today()
+            fc.put()
+
+
+    print "Moving", task.src_dir, "to processed directory"
+    os.rename(
+            task.src_dir,
+            os.path.join(nsc.PRIMARY_STORAGE, "processed", task.runid)
+            )
+
+    task.success_finish()
+
+
+with taskmgr.Task(TASK_NAME, TASK_DESCRIPTION, TASK_ARGS) as task:
+    main(task)
+
