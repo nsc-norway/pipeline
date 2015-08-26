8955ad0fb90ee45141dd6b9163e95df0a5b43d5c
Marius Bjoernstad <marius.bjornstad@medisin.uio.no>
2015-08-26 10:04:00 +0200

    copy the run from nextseq again, and check for completion

---
+++ 85_copy_run_check.py
@@ -0,0 +1,96 @@
+# Extra copy step for NextSeq. 
+
+# This copies the run from the NextSeq again, because the NextSeq will signal
+# that the run is finished before it's done washing. So there's a few log files
+# we're missing out on.
+
+import os
+import sys
+import getpass
+import time
+
+from genologics.lims import *
+from common import nsc, utilities, slurm, taskmgr
+
+TASK_NAME = "85. Copy run again"
+TASK_DESCRIPTION = "Check for completion and copy run metadata again"
+TASK_ARGS = ['src_dir', 'work_dir']
+
+nextseq_exclude_paths = [
+        "/Thumbnail_Images",
+        "/Data/Intensities/L00*",
+        "/Data/Intensities/BaseCalls/L00*",
+        ]
+
+
+def rsync_arglist(source_path, destination_path, exclude):
+    """Runs the rsync command. Note that trailing slashes
+    on paths are significant."""
+    
+    # See comments on rsync in 10_copy_run.py
+    args = [nsc.RSYNC, '-rLt', '--chmod=ug+rwX']
+    args += ["--exclude=" + path for path in exclude]
+    args += [source_path, destination_path]
+    return args
+
+
+
+def main(task):
+    """To be run from LIMS on the NSC data processing step"""
+
+    task.running()
+    runid = task.run_id
+
+
+    source = task.src_dir
+    destination = task.work_dir    
+
+    # Specify source with trailing slash to copy content
+    source = source.rstrip('/') + "/"
+
+    # Check destination
+    instrument = utilities.get_instrument_by_runid(runid)
+    if not instrument:
+        task.fail("Destination does not look like an Illumina run folder", 
+                """Remember to include the name of the destination directory in the 
+work_dir argument.""")
+
+    if instrument == "nextseq":
+        exclude = nextseq_exclude_paths
+    else:
+        task.info("This is a no-op for other sequencers than NextSeq.")
+        task.success_finish()
+
+    while not os.path.exists(os.path.join(source, "RunCompletionStatus.xml")):
+        task.info("Waiting for RunCompletionStatus.xml...")
+        time.sleep(60)
+
+
+    task.info("Copying remaining files...")
+    args = rsync_arglist(source, destination, exclude)
+    srun_args = ["--nodelist=loki"]
+    
+    logfile=task.logfile("rsync-2")
+    if task.process:
+        job_name = task.process.id + "." + TASK_NAME
+    else:
+        job_name = TASK_NAME
+
+    rc = slurm.srun_command(
+            args, job_name, "02:00:00", logfile=logfile, srun_user_args=srun_args,
+            change_user=getpass.getuser() == "glsai"
+            )
+    
+    if rc == 0:
+        task.success_finish()
+    else:
+        try:
+            detail = open(logfile).read()
+        except IOError:
+            detail = None
+        task.fail("rsync failed", detail)
+
+
+with taskmgr.Task(TASK_NAME, TASK_DESCRIPTION, TASK_ARGS) as task:
+    main(task)
+
