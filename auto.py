ae7a4c368c268f9254583e0e79763b941beb7867
Marius Bjoernstad <p.m.bjornstad@medisin.uio.no>
2015-08-26 12:47:43 +0200

    adding debugging output and fixes

--- auto.py
+++ auto.py
@@ -7,6 +7,7 @@
 # It only triggers scripts (emulates button presses) using the API to
 # manage the processing status. 
 
+import logging
 import re
 
 # scilife genologics library
@@ -15,14 +16,31 @@ from genologics.lims import *
 # Local
 from common import nsc, utilities
 
+def is_sequencing_finished(process):
+    seq_process = utilities.get_sequencing_process(process)
+    try:
+        return seq_process.udf['Finish Date']
+    except KeyError:
+        return False
+
 
 def start_programs():
     processes = nsc.lims.get_processes(type=nsc.DEMULTIPLEXING_QC_PROCESS)
+
     if not processes:
+        logging.debug("No processes found")
         return
 
     for process in processes:
-        seq_process = utilities.get_
+
+        logging.debug("Checking process " + process.id + "...")
+
+        logging.debug("Checking if sequencing is finished...")
+        if not is_sequencing_finished(process):
+            logging.debug("Wasn't.")
+            continue
+
+        logging.debug("Sequencing is finished, checking if we can start some jobs")
         try:
             state = process.udf[nsc.JOB_STATE_CODE_UDF]
         except KeyError:
@@ -33,36 +51,53 @@ def start_programs():
         elif state == None:
             previous_program = None
         else:
-            return # << return if state is not "COMPLETED" or None
+            logging.debug("Have to wait because program is in state " + str(state))
+            continue # skip to next if state is not "COMPLETED" or None
 
-            step = Step(nsc.lims, id=process.id)
-            if step.program_status == None or step.program_status.status == "OK":
-                auto_udf_match = [
-                        re.match(r"Auto ([\d-]+\..*)", udfname)
-                        for udfname, udfvalue in process.udf.items()
-                        if udfvalue
-                        ]
-                auto_udf_name = sorted(m.group(1) for m in auto_udf_match if m)
+        step = Step(nsc.lims, id=process.id)
+        if step.program_status == None or step.program_status.status == "OK":
+            auto_udf_match = [
+                    re.match(r"Auto ([\d-]+\..*)", udfname)
+                    for udfname, udfvalue in process.udf.items()
+                    if udfvalue
+                    ]
+            auto_udf_name = sorted(m.group(1) for m in auto_udf_match if m)
 
-                try:
-                    next_program = next(
-                            button_name for button_name in auto_udf_match
-                            if button_name > previous_program
-                            )
-                
-                    button = next(
-                            program 
-                            for program in step.available_programs
-                            if program.name == next_program
-                            )
+            try:
+                next_program = next(
+                        button_name for button_name in auto_udf_name
+                        if button_name > previous_program
+                        )
 
-                    button.trigger()
+            except StopIteration:
+                logging.debug("Couldn't find the next checkbox after " + str(previous_program)
+                        + ", is everything finished?")
 
-                except StopIteration:
-                    pass # Can't find next checkbox or button
+            try:
+                button = next(
+                        program 
+                        for program in step.available_programs
+                        if program.name == next_program
+                        )
+
+                logging.debug("Triggering " + next_program)
+                button.trigger()
+
+            except StopIteration:
+                logging.debug("Couldn't find the button for " + next_program)
+
+        else:
+            if step.program_status.status in ["RUNNING", "QUEUED"]:
+                logging.debug("A program is executing, skipping this process")
+            else:
+                logging.debug("There's a program in state " + 
+                        step.program_status.status + ", requires manual action")
 
 
 if __name__ == "__main__":
+    if nsc.TAG == "dev":
+        logging.basicConfig(level=logging.DEBUG)
+    logging.debug("auto.py Workflow management script")
     start_programs()
 
 
