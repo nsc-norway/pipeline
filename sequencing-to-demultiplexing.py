ee770fcd8ac4378fcfb588a32cf2080a8fdc0733
Marius Bjørnstad <p.m.bjornstad@medisin.uio.no>
2015-09-16 22:50:01 +0200

    added draft of automation automation (was committed to wrong branch)

--- sequencing-to-demultiplexing.py
+++ sequencing-to-demultiplexing.py
@@ -6,17 +6,21 @@ import time
 from genologics.lims import *
 from genologics import config
 
+PRESET = (
+        "hiseq": "HiSeq auto",
+        "miseq": "MiSeq auto",
+        "nextseq": "NextSeq auto"
+        }
+
+
 def start_step(lims, analytes, workflow):
     protocol = workflow.protocols[0]
     ps = protocol.steps[0]
     queue = ps.queue()
     for attempt in xrange(3):
         if set(analytes) <= set(queue.artifacts):
-            lims.create_step(ps, analytes)
-            break
+            return lims.create_step(ps, analytes)
         else:
-            print analytes
-            print queue.artifacts
             time.sleep(1)
             queue.get(force=True)
     else: # if not break
@@ -28,12 +32,14 @@ def main(username, password, process_id, workflow_name):
     # Sequencing process
     lims = Lims(config.BASEURI, username, password)
     process = Process(lims, id=process_id)
-    if workflow_name != "None":
-        workflows = lims.get_workflows(name=workflow_name)
-        workflow = workflows[0]
-        analytes = process.all_inputs(unique=True)
-        lims.route_analytes(analytes, workflow)
-        start_step(lims, analytes, workflow)
+    workflows = lims.get_workflows(name=workflow_name)
+    workflow = workflows[0]
+    analytes = process.all_inputs(unique=True)
+    lims.route_analytes(analytes, workflow)
+    step = start_step(lims, analytes, workflow)
+    instrument = utilities.get_instrument(process)
+    step.details.preset = PRESET[instrument]
+    step.details.put()
 
 
 main(*sys.argv[1:])
