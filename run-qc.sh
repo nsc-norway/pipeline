661a924ac830a0d857eaddb5b47a07caf4cb8dac
Marius Bjoernstad <marius.bjornstad@medisin.uio.no>
2015-11-20 10:19:20 +0100

    accepting extra options for scripts

--- run-qc.sh
+++ run-qc.sh
@@ -6,12 +6,12 @@ SCRIPTS="50_emails.py 60_fastqc.py 70_reports.py 70_demultiplex_stats.py 80_md5s
 
 if [ -z "$1" ]
 then
-	echo "Use: run-qc.sh <WORK-DIR>"
+	echo "Use: run-qc.sh [OPTIONS] <WORK-DIR>"
 	exit 1
 fi
 
 for script in $SCRIPTS
 do
-	python $(dirname $0)/$script $1
+	python $(dirname $0)/$script "$@"
 done
 
