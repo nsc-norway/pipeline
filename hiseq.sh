8705516c4a888bd72783f30eeeed0ea96fd84477
Marius Bjoernstad <p.m.bjornstad@medisin.uio.no>
2016-07-18 11:13:43 +0200

    Using the EXTRA_OPTIONS variable

--- hiseq.sh
+++ hiseq.sh
@@ -39,7 +39,7 @@ DIR=`dirname $0`
 
 python $DIR/10_copy_run.py $SOURCE $DEST
 python $DIR/20_prepare_sample_sheet.py $LANES $DEST
-python $DIR/30_demultiplexing.py $THREADS $LANES $SOURCE $DEST
+python $DIR/30_demultiplexing.py $THREADS $EXTRA_OPTIONS $LANES $SOURCE $DEST
 python $DIR/40_move_results.py $LANES $DEST
 python $DIR/50_emails.py $LANES $DEST
 python $DIR/60_fastqc.py $THREADS $LANES $DEST
