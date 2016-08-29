d34a98bffa9a7d0481675b98ea2fd0ec9d466d94
Marius Bjoernstad <p.m.bjornstad@medisin.uio.no>
2016-08-29 14:17:44 +0200

    Correcting set summary

--- 60_fastqc.py
+++ 60_fastqc.py
@@ -80,7 +80,7 @@ def main(task):
                 job.check_status()
             task.array_job_status(jobs)
         
-        if sum((set(job.summary.keys()) for job in jobs), set()) != set(("COMPLETED",)):
+        if set.union(*(set(job.summary.keys()) for job in jobs)) != set(("COMPLETED",)):
             task.fail("fastqc failure", str(aj.summary))
 	
     else:
