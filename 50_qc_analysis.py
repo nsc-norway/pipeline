6a1d8654e6624360ae4add0ac81542c3985fd94e
Marius Bjoernstad <p.m.bjornstad@medisin.uio.no>
2016-12-12 15:56:12 +0100

    Add comments to match jobs <-> run IDs

--- 50_qc_analysis.py
+++ 50_qc_analysis.py
@@ -89,6 +89,7 @@ def main(task):
             fqc_log_path.replace(".txt", ".%a.txt"))
     fqc.mem_per_task = 1900
     fqc.cpus_per_task = 1
+    fqc.comment = run_id
     jobs = []
 
     if task.instrument in ["hiseqx", "hiseq4k"] and nsc.FASTDUP != None:
@@ -96,6 +97,7 @@ def main(task):
                 dup_log_path.replace(".txt", ".%a.txt"))
         dup.mem_per_task = 500
         dup.cpus_per_task = 1
+        dup.comment = run_id
         jobs = [fqc, dup]
     else:
         jobs = [fqc]
