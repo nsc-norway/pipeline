41394639888eb5ee27357d25c10e06b63cc7e96a
Marius Bjoernstad <p.m.bjornstad@medisin.uio.no>
2017-03-06 09:50:06 +0100

    Fixed bash script bug

--- hiseq.sh
+++ hiseq.sh
@@ -24,25 +24,25 @@ done
 SOURCE=$1
 DEST=$2
 
-if [[ -z "$SOURCE" ]]
+if [[ -z ""$SOURCE"" ]]
 then
 	echo "Use: hiseq.sh [--lanes=XYZ] [--extra-options=OPTIONS] [--threads=N] SOURCE_DIR [DESTINATION_DIR]"
 	exit 1
 fi
 
-if [[ -z "$DEST" ]]
+if [[ -z ""$DEST"" ]]
 then
-    DEST=$SOURCE
+    DEST="$SOURCE"
 fi
 
 DIR=`dirname $0`
 
-python $DIR/10_copy_run.py $SOURCE $DEST
-python $DIR/20_prepare_sample_sheet.py $LANES $DEST
-python $DIR/30_demultiplexing.py $THREADS $EXTRA_OPTIONS $LANES $SOURCE $DEST
-python $DIR/40_move_results.py $LANES $DEST
-python $DIR/50_qc_analysis.py $THREADS $LANES $DEST
-python $DIR/60_emails.py $LANES $DEST
-python $DIR/60_reports.py $LANES $DEST
-python $DIR/80_md5sum.py $THREADS $LANES $DEST
+python $DIR/10_copy_run.py "$SOURCE" "$DEST"
+python $DIR/20_prepare_sample_sheet.py $LANES "$DEST"
+python $DIR/30_demultiplexing.py $THREADS "$EXTRA_OPTIONS" $LANES "$SOURCE" "$DEST"
+python $DIR/40_move_results.py $LANES "$DEST"
+python $DIR/50_qc_analysis.py $THREADS $LANES "$DEST"
+python $DIR/60_emails.py $LANES "$DEST"
+python $DIR/60_reports.py $LANES "$DEST"
+python $DIR/80_md5sum.py $THREADS $LANES "$DEST"
 
