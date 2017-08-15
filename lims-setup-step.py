f39317cff7b8793c5458c883ef79447f7032569f
Marius Bjoernstad <paalmbj@vali.ous.nsc.local>
2017-08-15 10:53:18 +0200

    Moving the "copy run again" after delivery script, so we don't have to wait

--- lims-setup-step.py
+++ lims-setup-step.py
@@ -55,7 +55,7 @@ else:
                 "No lane splitting",
                 "Auto 10. Copy run", "Auto 20. Prepare SampleSheet", "Auto 30. Demultiplexing",
                 "Auto 40. Move fastq files", "Auto 50-80. QC",
-                "Auto 85. Copy run again (NextSeq)", "Auto 90. Delivery and triggers",
+                "Auto 90. Delivery and triggers", "Auto 95. Copy run again (NextSeq)",
                 "Close when finished"
                 ],
             "hiseqx": [
