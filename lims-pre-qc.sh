fab56f7ea99b98d90bb600fd02d3eafec7afcb71
Marius Bjoernstad <p.m.bjornstad@medisin.uio.no>
2017-09-04 09:54:17 +0200

    Changed order of pre QC processes

--- lims-pre-qc.sh
+++ lims-pre-qc.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-/usr/bin/python /data/nsc.loki/automation/pipeline/40_move_results.py --pid=$1
+
+# Note: These must be in order for the process to continue
 /usr/bin/python /data/nsc.loki/automation/pipeline/40_copy_sav_files.py --pid=$1
+/usr/bin/python /data/nsc.loki/automation/pipeline/40_move_results.py --pid=$1
 
