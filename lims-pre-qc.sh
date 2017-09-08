d860d756ae2a2e8d7c61461b574ce60302fdf588
Marius Bjoernstad <p.m.bjornstad@medisin.uio.no>
2017-09-08 11:25:57 +0200

    Removing SAV script again ! will be moved into diag deliveries. Much better.

--- lims-pre-qc.sh
+++ lims-pre-qc.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-# Note: These must be in order for the process to continue
-/usr/bin/python /data/nsc.loki/automation/pipeline/40_copy_sav_files.py --pid=$1
 /usr/bin/python /data/nsc.loki/automation/pipeline/40_move_results.py --pid=$1
 
