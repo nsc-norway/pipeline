4635b12f9576bb760da86651c7b6739f069e0639
Marius Bjoernstad <p.m.bjornstad@medisin.uio.no>
2018-05-14 14:31:36 +0200

    change for different sample sheet format

--- tests/tools/anonymizer.py
+++ tests/tools/anonymizer.py
@@ -4,17 +4,18 @@ import random
 import sys
 alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 id_name = {}
+lane_offset = 0
 with open(sys.argv[1]) as f:
     for line in f:
         fiels = line.split(",")
-        if len(fiels) > 2 and fiels[2]:
+        if len(fiels) > 1+lane_offset and fiels[1+lane_offset]:
             try:
-                fiels[2] = id_name[fiels[1]]
+                fiels[1+lane_offset] = id_name[fiels[lane_offset]]
             except KeyError:
-                data = fiels[2].split("-")
-                fiels[2] = data[0]
+                data = fiels[1+lane_offset].split("-")
+                fiels[1+lane_offset] = data[0]
                 for d in data[1:]:
-                    fiels[2] += "-" + "".join(random.choice(alpha) for a in d)
-                id_name[fiels[1]] = fiels[2]
+                    fiels[1+lane_offset] += "-" + "".join(random.choice(alpha) for a in d)
+                id_name[fiels[lane_offset]] = fiels[1+lane_offset]
         print ",".join(fiels),
 
