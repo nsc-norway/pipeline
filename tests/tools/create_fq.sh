3a7075dc8e203c37e5d890a7f8ec5e53387ec157
Marius Bjoernstad <p.m.bjornstad@medisin.uio.no>
2018-05-14 14:50:40 +0200

    Cleanup, changed directory name

---
+++ tests/tools/create_fq.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Create fastq files in new dir with one byte gziped
+
+SOURCE_RUN=$1
+DEST_RUN=`readlink -f $2`
+
+pushd $SOURCE_RUN
+for f in `find -type f -name \*.fastq.gz`
+do
+    FPATH=$DEST_RUN/$f
+    mkdir -p `dirname $FPATH`
+    echo '' | gzip > $FPATH
+done
+popd
+
