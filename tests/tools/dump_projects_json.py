81780423df38f66ea6fe4bcd887b8c9875042530
Marius Bjoernstad <p.m.bjornstad@medisin.uio.no>
2018-05-29 16:57:16 +0200

    Added more runtime options using env vars

--- tests/tools/dump_projects_json.py
+++ tests/tools/dump_projects_json.py
@@ -5,25 +5,29 @@
 # The json-encoded representation can be used as a reference
 # for targeted tests of the sample sheet parsing.
 
-# Default settings for the sample sheet parser are provided, and
-# you can alter the parameters in this code if other settings
-# are required.
+# Default settings for the sample sheet parser can be set
+# using environment variables. See example.
 
 # USAGE:
 # python dump_projects_json.py SAMPLE_SHEET_PATH RUN_ID
 
+# EXAMPLE:
+# READS=1 COLLAPSE_LANES=true python dump_projects_json.py SampleSheet.csv 100000_RTEST
+
 import sys
+import os
 import json
 sys.path.append('..')
 from common import samples, nsc
 from test_module import projects_to_dicts
 
-collapse_lanes = False # This setting turns lane number into "X"
+collapse_lanes = os.environ.get('COLLAPSE_LANES') == 'true' # This setting turns lane number into "X"
+reads = int(os.environ.get('READS', 2)) # Number of data reads; 1=single read, 2=paired end
 
 with open(sys.argv[1]) as sample_sheet_file:
     sample_sheet_content = sample_sheet_file.read()
 sample_sheet = samples.parse_sample_sheet(sample_sheet_content)
 sample_sheet_data = sample_sheet['data']
-projects = samples.get_projects(sys.argv[2], sample_sheet_data, 2, collapse_lanes)
+projects = samples.get_projects(sys.argv[2], sample_sheet_data, reads, collapse_lanes)
 print(json.dumps(projects_to_dicts(projects)))
 
