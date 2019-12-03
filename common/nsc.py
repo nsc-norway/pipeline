7a43ed667e8552e7f8ff6cc1c18045f4a6b55c8c
Marius Bjoernstad <p.m.bjornstad@medisin.uio.no>
2019-12-03 16:21:57 +0100

    clean up obsolete config

--- common/nsc.py
+++ common/nsc.py
@@ -66,7 +66,6 @@ SEQ_PROCESSES=[
         ('hiseqx', 'Illumina Sequencing (HiSeq X) 1.0'),
         ('hiseq4k', 'Illumina Sequencing (HiSeq 3000/4000) 1.0'),
         ('novaseq', 'AUTOMATED - NovaSeq Run (NovaSeq 6000 v3.0)'),
-        ('hiseq', 'Illumina Sequencing (Illumina SBS) 5.0'),
         ('nextseq', 'NextSeq Run (NextSeq) 1.0'),
         ('miseq', 'MiSeq Run (MiSeq) 5.0'),
         ('miseq', 'MiSeq Run (MiSeq v1.0)')
