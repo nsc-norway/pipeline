4f4f155957b28a541b53571583b4ab6355020f0f
Marius Bjoernstad <marius.bjornstad@medisin.uio.no>
2021-10-08 12:22:28 +0200

    Allow to delete from diagnostics runs and real path boston

--- lims-delete-demultiplexing-outputs.sh
+++ lims-delete-demultiplexing-outputs.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 DIR=`echo "$1" | head -n1`
-if echo "$DIR" | grep -x '/data/runScratch\.boston/demultiplexed/[1-9][0-9]*_[^/]*/*' > /dev/null
+if echo "$DIR" | grep -x '/data/runScratch\.boston/demultiplexed/[1-9][0-9]*_[^/]*/*' > /dev/null || \
+            echo "$DIR" | grep -x '/boston/runScratch/demultiplexed/[1-9][0-9]*_[^/]*/*' > /dev/null || \
+            echo "$DIR" | grep -x '/boston/diag/runs/demultiplexed/[1-9][0-9]*_[^/]*/*' > /dev/null
 then
     if [ -d "$DIR" ]
     then
         rm -rf $DIR/Data/Intensities/BaseCalls/{Reports,Stats,QualityControl}
-        rm -rf $DIR/Data/Intensities/BaseCalls/{Reports,Stats,QualityControl}_L00*
+        rm -rf $DIR/Data/Intensities/BaseCalls/{Reports,Stats,QualityControl}_L*
         rm -rf $DIR/Data/Intensities/BaseCalls/*.Project_*
         rm -rf $DIR/Data/Intensities/BaseCalls/multiqc_*
         rm -rf $DIR/Data/Intensities/BaseCalls/Undetermined_*.fastq.gz
