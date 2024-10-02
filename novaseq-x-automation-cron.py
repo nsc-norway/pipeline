e66823de0fbdfe4ebda2a2b5f85511ab7c49cab9
Marius Bjørnstad <pmb@fa2k.net>
2024-10-02 17:50:27 +0200

    Add logging

--- novaseq-x-automation-cron.py
+++ novaseq-x-automation-cron.py
@@ -4,14 +4,48 @@ import sys
 from pathlib import Path
 import subprocess
 import os
+import logging
+
+
+DEMULTIPLEXED_RUNS_PATH = Path("/data/runScratch.boston/demultiplexed")
+SCRIPT_DIR_PATH = Path(__file__).resolve().parent
+INPUT_RUN_PATH = Path("/data/runScratch.boston/NovaSeqX")
+
+
+def setup_logging(analysis_path):
+    # Set up separate loggers for progress and errors
+    logfile_path = analysis_path / "automation_log_nsc.txt"
+    
+    # Progress logger (logs only to file)
+    progress_logger = logging.getLogger(f"progress_{analysis_path}")
+    progress_logger.setLevel(logging.INFO)
+    
+    file_handler = logging.FileHandler(logfile_path)
+    file_formatter = logging.Formatter('%(asctime)s - %(message)s')
+    file_handler.setFormatter(file_formatter)
+    
+    # Only log progress information to the file
+    progress_logger.addHandler(file_handler)
+    
+    # Exception logger (logs to both file and stderr)
+    error_logger = logging.getLogger(f"error_{analysis_path}")
+    error_logger.setLevel(logging.ERROR)
+    
+    # Console handler for stderr (for sending emails)
+    console_handler = logging.StreamHandler(sys.stderr)
+    console_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    console_handler.setFormatter(console_formatter)
+    
+    # Reuse the file handler for error logger (log to both stderr and file)
+    error_logger.addHandler(file_handler)
+    error_logger.addHandler(console_handler)
+    
+    return progress_logger, error_logger
 
 def main():
     os.umask(0o007)
-    input_run_path = Path("/data/runScratch.boston/NovaSeqX")
-    demultiplexed_runs_path = Path("/data/runScratch.boston/demultiplexed")
-    script_dir_path = Path(__file__).resolve().parent
 
-    for lims_file_path in input_run_path.glob("*/Analysis/*/ClarityLIMSImport_NSC.yaml"):
+    for lims_file_path in INPUT_RUN_PATH.glob("*/Analysis/*/ClarityLIMSImport_NSC.yaml"):
         analysis_path = lims_file_path.parents[0]
         run_id = lims_file_path.parents[2].name
 
@@ -24,43 +58,44 @@ def main():
                 lims_info = yaml.safe_load(f)
 
             if lims_info.get('status') != 'ImportCompleted':
-                continue
+                continue # If the LIMS import is not completed, we skip this path.
 
-            # Misc. setup
-            logfile = open(automation_log_path, 'w')
-            print("Started automation at", str(datetime.datetime.now()), file=logfile)
-            
-            demultiplexed_run_dir = demultiplexed_runs_path / run_id # Created by file mover if nsc projects
-            # Suffix for some directories created by the file mover script
-            if analysis_path.name == "1":
-                suffix = ""
-            else:
-                suffix = f"_{analysis_path.name}"
+            # Setup the loggers, also creating the log file, which is used as a flag to not rerun the automation.
+            progress_logger, error_logger = setup_logging(analysis_path)
+            try:
+                # Log progress
+                progress_logger.info(f"Started automation at {datetime.datetime.now()}")
 
-            # Run the file mover synchronously
-            subprocess.run(
-                ["nsc-python3", str(script_dir_path / "novaseq-x-file-mover.py"), str(analysis_path)],
-                check=True
-            )
-
-            # Project-specific processing
-            projects = set(sample['project_name'] for sample in lims_info['samples'])
-            nsc_project_slurm_jobs = []
-            any_diag_project = False
-            for project_name in projects:
-                print("Processing project", project_name, file=logfile)
-                project_samples = [sample for sample in lims_info['samples'] if sample['project_name'] == project_name]
-                project_type = project_samples[0]['project_type']
-                delivery_method = project_samples[0]['delivery_method'].replace(" ", "_")
-                if project_type == "Diagnostics":
-                    any_diag_project = True
+                demultiplexed_run_dir = DEMULTIPLEXED_RUNS_PATH / run_id
+                if analysis_path.name == "1":
+                    suffix = ""
                 else:
-                    job_id = start_nsc_nextflow(project_name, run_id, suffix, delivery_method, demultiplexed_run_dir)
-                    nsc_project_slurm_jobs.append(job_id)
+                    suffix = f"_{analysis_path.name}"
 
-            # Queue run-based processing if there are any NSC projects
-            if nsc_project_slurm_jobs:
-                run_slurm_script = f"""#!/bin/bash
+                # Run the file mover
+                subprocess.run(
+                    ["nsc-python3", str(SCRIPT_DIR_PATH / "novaseq-x-file-mover.py"), str(analysis_path)],
+                    check=True
+                )
+
+                # Project-specific processing
+                projects = set(sample['project_name'] for sample in lims_info['samples'])
+                nsc_project_slurm_jobs = []
+                any_diag_project = False
+                for project_name in projects:
+                    progress_logger.info(f"Processing project {project_name}")
+                    project_samples = [sample for sample in lims_info['samples'] if sample['project_name'] == project_name]
+                    project_type = project_samples[0]['project_type']
+                    delivery_method = project_samples[0]['delivery_method'].replace(" ", "_")
+                    if project_type == "Diagnostics":
+                        any_diag_project = True
+                    else:
+                        job_id = start_nsc_nextflow(project_name, run_id, suffix, delivery_method, demultiplexed_run_dir)
+                        nsc_project_slurm_jobs.append(job_id)
+
+                # Queue run-based processing
+                if nsc_project_slurm_jobs:
+                    run_slurm_script = f"""#!/bin/bash
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=8G
 #SBATCH --job-name=run-{run_id}
@@ -73,27 +108,31 @@ def main():
     --analysisid "Analysis{suffix}" \\
     --samplerenaminglist "SampleRenamingList-run.csv" 
 """
-                dependency_list = "afterany:" + ":".join(nsc_project_slurm_jobs)
-                pipeline_dir = demultiplexed_run_dir / "pipeline" / "run"
-                pipeline_dir.mkdir(parents=True, exist_ok=True)
-                slurm_script_path = pipeline_dir / "script.sh"
-                with open(slurm_script_path, 'w') as slurm_script_file:
-                    slurm_script_file.write(run_slurm_script)
-                subprocess.run(
+                    dependency_list = "afterany:" + ":".join(nsc_project_slurm_jobs)
+                    pipeline_dir = demultiplexed_run_dir / "pipeline" / "run"
+                    pipeline_dir.mkdir(parents=True, exist_ok=True)
+                    slurm_script_path = pipeline_dir / "script.sh"
+                    with open(slurm_script_path, 'w') as slurm_script_file:
+                        slurm_script_file.write(run_slurm_script)
+                    subprocess.run(
                         ["sbatch", "--dependency=" + dependency_list, str(slurm_script_path)],
                         cwd=pipeline_dir,
                         check=True,
                         stdout=subprocess.DEVNULL
-                )
+                    )
 
-            # Finally run the diagnostics processing job. Done last because this is blocking on the md5sum jobs.
-            if any_diag_project:
-                subprocess.run(
-                    ["nsc-python3", str(script_dir_path / "novaseq-x-diag.py"), str(lims_file_path)],
-                    check=True
-                )
+                if any_diag_project:
+                    subprocess.run(
+                        ["nsc-python3", str(SCRIPT_DIR_PATH / "novaseq-x-diag.py"), str(lims_file_path)],
+                        check=True
+                    )
 
-            print("Completed automation at", str(datetime.datetime.now()), file=logfile)
+                progress_logger.info(f"Completed automation at {datetime.datetime.now()}")
+                here = error(now)
+    
+            except Exception as e:
+                error_logger.error(f"Exception occurred: {e}", exc_info=True)
+                raise  # Re-raise the exception to trigger any necessary stderr email output
 
 
 def start_nsc_nextflow(project_name, run_id, suffix, delivery_method, demultiplexed_run_dir):
@@ -124,7 +163,6 @@ def start_nsc_nextflow(project_name, run_id, suffix, delivery_method, demultiple
         stdout=subprocess.PIPE).stdout.decode().strip()
     
     return job_id
-
 
 if __name__ == "__main__":
     main()
